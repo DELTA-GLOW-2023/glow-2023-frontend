5631dda1854066f81239c8e8c1134cacf207447f
Kalle Pronk <kallepronk@gmail.com>
2023-05-24 08:46:21 +0200

    Changed image service to send two prompts to backend

--- src/services/processImageService.ts
+++ src/services/processImageService.ts
@@ -9,17 +9,21 @@ export const ProcessImage = async (
   setting: string,
   style: string
 ): Promise<ImageResponseType> => {
-  const actorPrompt: string =
-    actors.find((x) => x.title === actor)?.prompt || "";
-  const settingPrompt: string =
-    settings.find((x) => x.title === setting)?.prompt || "";
-  const stylePrompt: string =
+  const selectedActor: any = actors.find((x) => x.title === actor) || "";
+  const selectedSetting: any = settings.find((x) => x.title === setting) || "";
+  const selectedStyle: string =
     styles.find((x) => x.title === style)?.prompt || "";
 
-  const prompt = `transform the person in the image into: ${actorPrompt}, change the background scenery into: ${settingPrompt}, ${stylePrompt}`;
+  const prompt = `transform the person in the image into: ${selectedActor?.prompt}, change the background scenery into: ${selectedSetting?.prompt}, ${selectedStyle}`;
+  const promptDescription = `${selectedActor.title} ${selectedSetting.title}`;
+  const secondPrompt = `transform the person in the image into: ${selectedActor?.secondPrompt}, change the background scenery into: ${selectedSetting?.secondPrompt}, ${selectedStyle}`;
+  const secondPromptDescription = `${selectedActor?.secondTitle} ${selectedSetting?.secondTitle}`;
   const json = {
     image: image,
     prompt: prompt,
+    promptDescription: promptDescription,
+    secondPrompt: secondPrompt,
+    secondPromptDescription: secondPromptDescription,
   };
   const { data } = await axios.post<ImageResponseType>(
     `${API_URL}/process-image`,
