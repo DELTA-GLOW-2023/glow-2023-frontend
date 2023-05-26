1cd58fc663042a198f6d3d2a9de09b03bcf2e5d9
Bart van Eijkelenburg <bartvaneijkelenburg@outlook.com>
2023-05-26 15:00:13 +0200

    Delete Spinner.tsx

--- src/components/core/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-import { RiseLoader } from "react-spinners";
-
-export const Spinner: FC<{ size?: number; color?: string }> = ({
-  size = 100,
-  color = "#FFFFFF",
-}) => {
-  return <RiseLoader color={color} size={size} />;
-};
