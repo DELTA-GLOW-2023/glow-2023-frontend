ee60ae6384bce25bba0b7d7ad9fcedcba75031e5
Bart van Eijkelenburg <bartvaneijkelenburg@outlook.com>
2023-05-26 14:35:31 +0200

    markup fix

--- src/services/displayImageService.ts
+++ src/services/displayImageService.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import { API_URL } from "../config/config";
 
 export const DisplayImage = async () => {
-  const { data } = await axios.get(
-    `${API_URL}/view-image/display`
-  );
+  const { data } = await axios.get(`${API_URL}/view-image/display`);
   return data;
 };
