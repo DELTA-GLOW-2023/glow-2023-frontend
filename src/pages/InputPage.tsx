95b9025758621b9da0b7d8cf5015fa9ce35da6ba
DewiKimC <97511825+DewiKimC@users.noreply.github.com>
2023-10-09 12:48:17 +0200

    Returns error when a vulgar word is detected

--- src/pages/InputPage.tsx
+++ src/pages/InputPage.tsx
@@ -6,18 +6,28 @@ import { ProcessImage } from "../services/processImageService.ts";
 import Carousel from "../components/core/Carousel.tsx";
 import "react-simple-keyboard/build/css/index.css";
 import { KeyboardComponent } from "../components/core/KeyboardComponent.tsx";
+import { AxiosError } from "axios";
 
 export const InputPage: FC = () => {
   const [loading, setLoading] = useState(false);
   const [showKeyBoard, setShowKeyBoard] = useState(false);
   const [customPrompt, setCustomPrompt] = useState("");
+  const [error, setError] = useState(true);
   const handleClick = async (prompt: string) => {
     localStorage.setItem("lastPrompt", prompt);
     setLoading(true);
-    await ProcessImage(prompt);
-    setLoading(false);
+    try {
+      await ProcessImage(prompt);
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        if (error.response?.data.includes("Vulgar words detected!")) {
+          setError(true);
+        }
+      }
+    } finally {
+      setLoading(false);
+    }
   };
-
   const activateKeyBoard = () => {
     setShowKeyBoard((prev) => !prev);
     // textarea element class h-20 remove
@@ -97,8 +107,12 @@ export const InputPage: FC = () => {
         What would you like to add?
       </motion.h1>
 
-      <div className={`glass mt-2 rounded-xl flex justify-center ${ showKeyBoard ? "py-16 w-[90vw]" : "p-16 w-[90vw]"} `}>
-        <div className={`${ showKeyBoard ? "" : "w-4/5"}`}>
+      <div
+        className={`glass mt-2 rounded-xl flex justify-center ${
+          showKeyBoard ? "py-16 w-[90vw]" : "p-16 w-[90vw]"
+        } `}
+      >
+        <div className={`${showKeyBoard ? "" : "w-4/5"}`}>
           <div className={"w-full mb-8"}>
             {!showKeyBoard ? (
               <button
@@ -112,36 +126,84 @@ export const InputPage: FC = () => {
             ) : (
               <div className={"flex flex-col items-center"}>
                 <div className={"w-full flex justify-center px-10"}>
-                    <div className={"absolute right-8 top-0 mt-16 w-20"}>
-                        <button
-                            className={
-                                "flex justify-center items-center h-20 w-full focus:outline-none bg-white bg-opacity-20 rounded-lg text-5xl text-left text-white"
-                            }
-                            onClick={ () => {
-                                setShowKeyBoard(false);
-                            }}
-                        >
-                          <svg width="100" height="100" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M16 8L8 16M8.00001 8L16 16" stroke="#fff"/>
-                          </svg>
-                        </button>
-                    </div>
-
-                  <textarea
+                  <div className={"absolute right-8 top-0 mt-16 w-20"}>
+                    <button
                       className={
-                        "secondary-font transition-all ease-in-out delay-300 focus:outline-none w-4/5 h-40 bg-white bg-opacity-20 rounded-lg text-4xl text-left text-white px-8 pt-4 text-opacity-50"
+                        "flex justify-center items-center h-20 w-full focus:outline-none bg-white bg-opacity-20 rounded-lg text-5xl text-left text-white"
+                      }
+                      onClick={() => {
+                        setShowKeyBoard(false);
+                      }}
+                    >
+                      <svg
+                        width="100"
+                        height="100"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M16 8L8 16M8.00001 8L16 16" stroke="#fff" />
+                      </svg>
+                    </button>
+                  </div>
+                  <div
+                    className={
+                      "flex flex-col justify-center items-center w-full"
+                    }
+                  >
+                    <textarea
+                      className={
+                        "secondary-font transition-all ease-in-out delay-300 focus:outline-none w-4/5 h-40 bg-white bg-opacity-20 rounded-lg text-4xl text-left text-white px-8 pt-4 text-opacity-50" +
+                        (error
+                          ? " border-solid outline outline-offset-4 outline-4 outline-red-400"
+                          : "")
                       }
                       readOnly={true}
-                      placeholder="Write your own prompt here..."
+                      placeholder={"Write your own prompt here..."}
                       value={customPrompt}
-                  />
+                    />
+                    <div className={"  text-white text-4xl mt-4"}>
+                      {error ? (
+                        <div className={"flex"}>
+                          <svg
+                            height="2.5rem"
+                            width="3.5rem"
+                            version="1.1"
+                            id="_x32_"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                            fill="#ffffff"
+                          >
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g
+                              id="SVGRepo_tracerCarrier"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></g>
+                            <g id="SVGRepo_iconCarrier">
+                              {" "}
+                              <style type="text/css"> </style>{" "}
+                              <g>
+                                {" "}
+                                <path
+                                  className="st0"
+                                  d="M505.095,407.125L300.77,53.208c-9.206-15.944-26.361-25.849-44.774-25.849 c-18.412,0-35.552,9.905-44.751,25.849L6.905,407.109c-9.206,15.944-9.206,35.746,0,51.69 c9.206,15.944,26.354,25.842,44.758,25.842h408.674c18.405,0,35.568-9.897,44.759-25.842 C514.302,442.855,514.302,423.053,505.095,407.125z M256.004,426.437c-17.668,0-32.013-14.33-32.013-32.004 c0-17.668,14.345-31.997,32.013-31.997c17.667,0,31.997,14.329,31.997,31.997C288.001,412.108,273.671,426.437,256.004,426.437z M275.72,324.011c0,10.89-8.834,19.709-19.716,19.709c-10.898,0-19.717-8.818-19.717-19.709l-12.296-144.724 c0-17.676,14.345-32.005,32.013-32.005c17.667,0,31.997,14.33,31.997,32.005L275.72,324.011z"
+                                ></path>{" "}
+                              </g>{" "}
+                            </g>
+                          </svg>
+                          Error
+                        </div>
+                      ) : null}
+                    </div>
+                  </div>
                 </div>
                 <button
                   className={
                     "secondary-font glass h-20 mt-10 focus:outline-none bg-white bg-opacity-20 rounded-lg text-5xl text-left text-white px-14"
                   }
                   onClick={async () => {
-                    if(customPrompt === "") return;
+                    if (customPrompt === "") return;
                     await handleClick(customPrompt);
                   }}
                 >
@@ -153,7 +215,12 @@ export const InputPage: FC = () => {
           <div>
             {showKeyBoard ? (
               <div className={""}>
-                <KeyboardComponent onChangeValue={setCustomPrompt} />
+                <KeyboardComponent
+                  onChangeValue={(val: string) => {
+                    setCustomPrompt(val);
+                    if (error) setError(false);
+                  }}
+                />
               </div>
             ) : (
               <Carousel>{showCards()}</Carousel>
