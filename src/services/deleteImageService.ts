282df9a252f9628a8f168508605869bc9ab2dafb
Bart van Eijkelenburg <bartvaneijkelenburg@outlook.com>
2023-11-07 11:30:37 +0100

    add authorization header to panic button

--- src/services/deleteImageService.ts
+++ src/services/deleteImageService.ts
@@ -2,8 +2,14 @@ import axios from "axios";
 import { API_URL } from "../config/config.ts";
 
 export const DeleteImage = async (): Promise<{ message: string }> => {
-	const { data } = await axios.post<{ message: string }>(
-		`${API_URL}/delete-image/`,
-	);
-	return data;
+  const { data } = await axios.post<{ message: string }>(
+    `${API_URL}/delete-image/`,
+    {},
+    {
+      headers: {
+        Authorization: localStorage.getItem("API_KEY"),
+      },
+    }
+  );
+  return data;
 };
