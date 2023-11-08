2dc3b6312c93daea31e7efb3a1f0172a6e140624
Bart van Eijkelenburg <bartvaneijkelenburg@outlook.com>
2023-11-08 10:01:04 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:4000";
 export const REMOTE_API_URL =
-  import.meta.env.VITE_REMOTE_API_URL ?? "http://localhost:9091";
+  import.meta.env.VITE_REMOTE_API_URL ?? "http://localhost:3001";
