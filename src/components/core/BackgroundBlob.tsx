f82db7eac3281a50f42d4a242190a80b7ffcf016
Bart van Eijkelenburg <bartvaneijkelenburg@outlook.com>
2023-11-10 11:10:50 +0100

    new blob

--- src/components/core/BackgroundBlob.tsx
+++ src/components/core/BackgroundBlob.tsx
@@ -1,11 +1,33 @@
 import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
 
+import blobVideo from "../../assets/blob.mp4"; // Import the MP4 video
+
 export const BackgroundBlob: React.FC<{
   children?: ReactNode;
 }> = ({ children }) => {
   return (
-    <motion.div className="overflow-x-hidden inset-0 container-background">
+    <motion.div
+      className="relative overflow-x-hidden inset-0 container-background"
+      style={{ height: "100vh" }}
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: -1, // Set a lower z-index to place it behind the children
+        }}
+      >
+        <source src={blobVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       {children}
     </motion.div>
   );
