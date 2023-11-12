08bd4ced02d89c04b5eee79265370d9e6cc22ab0
xl3ehindTim <timbrouwers1@gmail.com>
2023-11-12 13:17:36 +0100

    Added marquee animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ export default {
   theme: {
     fontFamily:{
       sans: ["Jura", "sans-serif"]
-    }
+    },
+    extend: {
+      animation: {
+        marquee: 'marquee 13s linear infinite',
+        marquee2: 'marquee2 13s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
